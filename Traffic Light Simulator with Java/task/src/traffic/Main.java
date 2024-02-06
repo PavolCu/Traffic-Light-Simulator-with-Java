1d2611730bcd46f1739fc419c7c8df7a50df4f33
Pavol Cuninka <pavol.cuninka@icloud.com>
2024-02-06 15:26:49 +0100

    modified  Main.java

--- Traffic Light Simulator with Java/task/src/traffic/Main.java
+++ Traffic Light Simulator with Java/task/src/traffic/Main.java
@@ -1,17 +1,23 @@
 package traffic;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args){
-
-
-
+    Scanner scanner = new Scanner(System.in);
+    Menu menu = new Menu(scanner);
+    TrafficSystem trafficSystem = new TrafficSystem();
 
     System.out.println("Welcome to the traffic management system!");
-    System.out.println("""
-            Menu:
-            1.Add
-            2.Delete
-            3.System
-            0.Quit """);
+
+    while (true) {
+      MenuOption = menu.getOption();
+
+      if(option == MenuOption.QUIT) {
+        break;
+      }
+
+      trafficSystem.handleOption(option);
+    }
   }
 }
