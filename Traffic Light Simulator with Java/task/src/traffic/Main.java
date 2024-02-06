a4e147181783066d39d77aa354f38195e052b141
Pavol Cuninka <pavol.cuninka@icloud.com>
2024-02-06 21:08:23 +0100

    modified main java added numberofRoads and interval inputing and printing

--- Traffic Light Simulator with Java/task/src/traffic/Main.java
+++ Traffic Light Simulator with Java/task/src/traffic/Main.java
@@ -5,10 +5,17 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
+    System.out.println("Welcome to the traffic management system!");
+
+    System.out.println("Please enter the desired number of roads:");
+    int numberofRoads = scanner.nextInt();
+
+    System.out.println("Please enter the interval at which the roads should open/closed:");
+    int interval = scanner.nextInt();
+
     Menu menu = new Menu(scanner);
     TrafficSystem trafficSystem = new TrafficSystem();
 
-    System.out.println("Welcome to the traffic management system!");
 
     while (true) {
       MenuOption option = menu.getOption();
